2480b1248945ad4bc9aca51c14416d7ea933dca8
Jim <gruber.james27@gmail.com>
2017-09-03 20:14:47 -0400

    almost done

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -9,6 +9,63 @@ var fs = require('fs'),
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
+mongoose.connect(config.db.uri);
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  if (err) throw err;
+  var listingData = JSON.parse(data);
+  for(var i in listingData.entries){
+    var newListing = listingData.entries[i];
+    if(newListing.coordinates && newListing.address){
+      new Listing({
+        name: newListing.name,
+        code: newListing.code,
+        address: newListing.address,
+        coordinates: {
+          latitude: newListing.coordinates.latitude,
+          longitude: newListing.coordinates.longitude
+        }
+      }).save(function(err, listing){
+        if (err) throw err;
+        console.log('Listing created!');
+      });
+    }
+    else if(newListing.address){
+      new Listing({
+        name: newListing.name,
+        code: newListing.code,
+        address: newListing.address,
+      }).save(function(err, listing){
+        if (err) throw err;
+        console.log('Listing created!');
+      });
+    }
+    else if(newListing.coordinates){
+      new Listing({
+        name: newListing.name,
+        code: newListing.code,
+        coordinates: {
+          latitude: newListing.coordinates.latitude,
+          longitude: newListing.coordinates.longitude
+        }
+      }).save(function(err, listing){
+        if (err) throw err;
+        console.log('Listing created!');
+      });
+    }
+    else{
+      new Listing({
+        name: newListing.name,
+        code: newListing.code,
+      }).save(function(err, listing){
+        if (err) throw err;
+        console.log('Listing created!');
+      });
+    }
+  }
+});
+
+
 /* Connect to your database */
 
 /* 
--- queries.js
+++ queries.js
@@ -1,12 +1,23 @@
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
+  Listing.find({code: 'LBW' }, function(err, listing){
+    if (err) throw err;
+    console.log(listing);
+  });
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
 };
 var removeCable = function() {
+  Listing.find({code: 'CABL' }, function(err, listing){
+    if (err) throw err;
+    listing.remove(function(err) {
+      if (err) throw err;
+      console.log('CABL deleted!');
+    });
+  });
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
@@ -14,12 +25,25 @@ var removeCable = function() {
    */
 };
 var updatePhelpsLab = function() {
+  Listing.find({code: 'PHL' }, function(err, listing){
+    if (err) throw err;
+    listing.address = '432 Newell Dr, Gainesville, FL 32611, United States';
+
+    user.save(function(err){
+      if (err) throw err;
+      console.log('PHL address updated!');
+    });
+  });
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
 };
 var retrieveAllListings = function() {
+  Listing.find({}, function(err, listings) {
+    if (err) throw err;
+    console.log(listings);
+  });
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
